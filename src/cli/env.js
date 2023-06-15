71e57c7f4d853970389778ee902ef649cd2749a3
DzmitryYesis <yesis1988@mail.ru>
2023-06-15 15:07:23 +0200

    feat: create function for parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const prefix = "RSS_"
+    const env = process.env
+    for (let key in env) {
+        if (key.startsWith(prefix)) {
+            console.log(`${key}=${env[key]}`)
+        }
+    }
 };
 
 parseEnv();
