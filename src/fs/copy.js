f3ffefa880cbb59509edd73388b79b85b021217f
DzmitryYesis <yesis1988@mail.ru>
2023-06-15 14:27:21 +0200

    feat: create function for copy folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from "node:fs"
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+    const folderPath = "src/fs/files/"
+    const copyFolderPath = "src/fs/files_copy/"
+
+    const isFileFolder = fs.existsSync(folderPath)
+    const isFileCopyFolder = fs.existsSync(copyFolderPath)
+
+    if (isFileFolder && !isFileCopyFolder) {
+        await fs.cp(folderPath, copyFolderPath, {recursive: true}, (error) => {
+        })
+    } else {
+        throw new Error("FS operation failed")
+    }
 };
 
 await copy();
