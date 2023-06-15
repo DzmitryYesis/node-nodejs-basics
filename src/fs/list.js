88040c5af87ec3d7eeaf22ca2f69c7ea9d8e62ab
DzmitryYesis <yesis1988@mail.ru>
2023-06-15 13:54:17 +0200

    feat: create function for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import fs from "node:fs"
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    await fs.readdir("src/fs/files/", (error, files) => {
+        if (error) {
+            throw new Error("FS operation failed")
+        } else {
+            files.forEach(file => console.log(file))
+        }
+    })
 };
 
 await list();
