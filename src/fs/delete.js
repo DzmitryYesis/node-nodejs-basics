781654565bb7adf0b3edfcd6984767ab2c56a5c4
DzmitryYesis <yesis1988@mail.ru>
2023-06-15 13:38:06 +0200

    feat: create function for delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "node:fs"
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+
+    fs.unlink("src/fs/files/fileToRemove.txt", (error) => {
+        if (error) {
+            if (error.code === "ENOENT") {
+                throw new Error("FS operation failed")
+            }
+        } else {
+            console.log("File deleted successfully")
+        }
+    })
+
 };
 
 await remove();
