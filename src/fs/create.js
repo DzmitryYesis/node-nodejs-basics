a2a414a6f8354a138e190ab3ac9c2614d6efaa34
DzmitryYesis <yesis1988@mail.ru>
2023-06-15 13:19:36 +0200

    feat: create function for creat file

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,7 +2,15 @@ import fs from 'node:fs';
 
 const create = async () => {
     // Write your code here
-
+    await fs.access("src/fs/files/fresh.txt", fs.constants.F_OK, (error) => {
+        if (!error) {
+            throw new Error("FS operation failed")
+        } else {
+            const text = "I am fresh and young"
+            const filePath = "src/fs/files/fresh.txt"
+            fs.writeFile(filePath, text, (error)=>{})
+        }
+    })
 };
 
 await create();
