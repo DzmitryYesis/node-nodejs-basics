87aceb9cf9fbda39fe0f842710735d70887d056c
DzmitryYesis <yesis1988@mail.ru>
2023-06-21 13:49:51 +0200

    feat: create function for calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import {createHash} from "node:crypto"
+import {fileURLToPath} from "node:url";
+import fs from "node:fs"
+
+
+const __filename = fileURLToPath(import.meta.url)
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+
+    fs.readFile(__filename, (err, data)=>{
+        if(err) throw err
+        const hash = createHash("SHA256")
+        hash.update(data)
+        console.log(hash.digest("hex"))
+    })
 };
 
 await calculateHash();
