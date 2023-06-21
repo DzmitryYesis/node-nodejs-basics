c34b5aa48d06b0d5db765ee2231d72e5b594b603
DzmitryYesis <yesis1988@mail.ru>
2023-06-21 15:18:06 +0200

    feat: create function for decompress file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import {fileURLToPath} from "node:url"
+import path, {dirname} from "node:path"
+import {createWriteStream, createReadStream} from "node:fs"
+import zlib from "node:zlib"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const pathToDecompressFile = path.join(__dirname, "files", "fileToCompress.txt")
+const pathToCompressFile = path.join(__dirname, "files", "archive.gz")
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const readStream = createReadStream(pathToCompressFile)
+    const writeStream = createWriteStream(pathToDecompressFile)
+    const gzipStream = zlib.createGunzip()
+
+    readStream.pipe(gzipStream).pipe(writeStream)
 };
 
 await decompress();
