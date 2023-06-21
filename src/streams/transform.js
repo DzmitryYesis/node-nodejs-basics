4868c76672b1c829e52d8233705c48d9b8be12f7
DzmitryYesis <yesis1988@mail.ru>
2023-06-21 14:53:14 +0200

    feat: create function for stream transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import {Transform} from "node:stream"
+
+const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+        const data = chunk.toString().trim().split("").reverse().join("")
+        callback(null, `${data}\n`)
+    }
+})
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    process.stdin.pipe(reverseStream).pipe(process.stdout)
 };
 
 await transform();
